9e60634e73a804d84abb5d2dbfd68a9466444f58
halixiedun <1158412582@qq.com>
2025-10-18 17:33:03 +0800

    修复博客界面显示不成功的问题

--- iproject/urls.py
+++ iproject/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.conf import settings
 from django.conf.urls.static import static
 
